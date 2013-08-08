9d505916387906347b2f90aadbefe7e52aa4c894
Owner <Owner@Computer-0668>
2013-08-07 21:41:49 -0300

    Criando a classe Janela Produto

---
+++ Revisa/src/br/senai/sc/view/JanelaProduto.java
@@ -0,0 +1,74 @@
+package br.senai.sc.view;
+
+
+import br.senai.sc.model.negocio.Produto;
+import br.senai.sc.persistencia.ProdutoDAO;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Owner
+ */
+public class JanelaProduto {
+     public void menuProduto(List<Produto>produtos) {
+        int opcao = 0;
+        do {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Produto \n"
+                    +"Informe a opção desejada:"
+                    + "\n1 - Inserir"
+                    + "\n2 - Excluir"
+                    + "\n3 - Alterar"
+                    + "\n4 - Buscar"
+                    + "\n5 - Listar"
+                    + "\n6 - Sair"));
+            switch(opcao){
+                case 1 :
+                   adicionaProduto(produtos);                                
+                   break;
+                case 2:
+                    removeProduto(produtos);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    ListaProduto(produtos);
+                    break;
+                case 6:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Você digitou uma opção incorreta");
+                
+            }
+        } while (opcao != 6);
+      }
+        void adicionaProduto(List<Produto> Produtos){            
+            Produto p  = new Produto();
+            
+            p.setNome(JOptionPane.showInputDialog("informe o nome do produto"));
+            p.setDataValidade(JOptionPane.showInputDialog("Informe a data de validade do funcionário"));
+            p.setValor(Double.parseDouble(JOptionPane.showInputDialog("Informe o valor do produto")));
+                                                                                       
+                                                                                                     
+           ProdutoDAO dao = new ProdutoDAO();
+                    dao.adicionar(p, Produtos);
+          
+      }
+
+    private void ListaProduto(List <Produto> produtos) {
+        ProdutoDAO dao = new ProdutoDAO();
+        dao.listar(produtos);
+    }
+
+    private void removeProduto(List<Produto> produtos) {
+        ProdutoDAO dao = new ProdutoDAO();
+        String nome = JOptionPane.showInputDialog
+                ("Informe o nome do produto a ser excluído");
+        
+      dao.excluir(nome, produtos);
+      
+     
+    }
+}

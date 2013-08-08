23a46bcde6ac3c17efee78f0be5cebbdadc0d1a9
Owner <Owner@Computer-0668>
2013-08-07 21:41:32 -0300

    Criando a classe produtoDAo e adiciona os atributos e métodos nela.

---
+++ Revisa/src/br/senai/sc/persistencia/ProdutoDAO.java
@@ -0,0 +1,28 @@
+package br.senai.sc.persistencia;
+import br.senai.sc.model.negocio.Produto;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class ProdutoDAO {
+
+    public void adicionar(Produto p, List<Produto> produtos) {
+        produtos.add(p);
+        JOptionPane.showMessageDialog(null, "Produto "
+                + "adicionado com sucesso. ");
+    }
+
+    public void listar(List<Produto> produtos) {
+        String msg = " ";
+        for (Produto p : produtos) {
+            msg = msg + "\nNome: " + p.getNome()
+                    + "\nData de validade do produto:" + p.getDataValidade()
+                    + "\nValor do produto: " + p.getValor();
+
+        }
+        JOptionPane.showMessageDialog(null, msg);
+    }
+
+    public void excluir(String nome, List<Produto> produtos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}

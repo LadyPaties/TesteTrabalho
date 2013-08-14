b66d12489b2754e9412cfd11cf241aad9301f46f
Owner <Owner@Computer-0668>
2013-08-14 20:57:11 -0300

    Teste para o trabalho de API Math

---
+++ TesteTrabalho/src/PotenciasRaizes.java
@@ -0,0 +1,10 @@
+public class PotenciasRaizes {
+    public static void main(String[] args) {
+System.out.println("1 MB tem " + Math.pow(2, 10) + " KB");
+System.out.println("Raiz cúbica de 1331 também é " + Math.cbrt(1331));
+System.out.println("A raiz quadrada de Pi é = "+ Math.sqrt( Math.PI ));
+System.out.println("O logaritmo natural de 10 é = "+ Math.log(10) );
+System.out.println("O logaritmo natural de 'e' é = "+ Math.log( Math.E ));
+    }
+   }
+
